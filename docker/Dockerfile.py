dfd0e04d04eb366725fd7e15fb49020b0821b797
Angela Deaguiar <angela.deaguiar@165gc.onmicrosoft.com>
2019-10-01 08:53:29 -0300

    First version of dockerfile for V4

---
+++ docker/Dockerfile.py
@@ -0,0 +1,10 @@
+FROM cccs/assemblyline-v4-service-base:latest
+
+ENV SERVICE_PATH virustotal_dynamic.VirusTotalDynamic
+
+# Switch to assemblyline user
+USER assemblyline
+
+# Copy VirusTotalStatic service code
+WORKDIR /opt/al_service
+COPY . .
