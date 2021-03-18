80be75390cc9cd5cc4a5064d782dea985b59bfce
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2021-03-18 08:16:01 -0700

    Updated to use vt-py library

--- virustotal_dynamic.py
+++ virustotal_dynamic.py
@@ -1,20 +1,10 @@
-import json
-import time
+import json, time
 from typing import Dict, Any
-
-import requests
+from vt import Client, APIError
 
 from assemblyline_v4_service.common.base import ServiceBase
 from assemblyline_v4_service.common.request import ServiceRequest
 from assemblyline_v4_service.common.result import Result, ResultSection, Classification, BODY_FORMAT
-
-
-class VTException(Exception):
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return repr(self.value)
 
 
 class AvHitSection(ResultSection):
@@ -35,126 +25,62 @@ class AvHitSection(ResultSection):
 class VirusTotalDynamic(ServiceBase):
     def __init__(self, config=None):
         super(VirusTotalDynamic, self).__init__(config)
-        self.api_key = self.config.get("api_key")
-        self.private_api = self.config.get("private_api")
+        self.client = None
 
     def start(self):
         self.log.debug("VirusTotalDynamic service started")
 
     def execute(self, request: ServiceRequest):
-        filename = request.file_path
-        response = self.scan_file(request, filename)
-        result = self.parse_results(response)
-        if self.private_api:
-            # Call some private API functions
-            pass
+        try:
+            self.client = Client(apikey=self.config.get("api_key", request.get_param("api_key")))
+        except Exception as e:
+            self.log.error("No API key found for VirusTotal")
+            raise e
 
+        response = self.scan_file(request)
+        result = self.parse_results(response)
         request.result = result
 
     # noinspection PyUnusedLocal
-    def scan_file(self, request: ServiceRequest, filename: str):
-        api_key = request.get_param('api_key', None)
-
-        # Let's scan the file
-        url = self.config.get("base_url") + "file/scan"
-
-        files = dict(file=open(filename, "rb"))
-        params = dict(apikey=api_key or self.api_key)
-
+    def scan_file(self, request: ServiceRequest):
+        filename = request.file_path
         json_response = None
-        try:
-            r = requests.post(url, params=params, files=files)
-            r.raise_for_status()
-
-            if r.ok:
-                json_response = r.json()
-            elif r.status_code == 204:
-                message = "You exceeded the public API request rate limit (4 requests of any nature per minute)"
-                raise VTException(message)
-        except requests.ConnectionError:
-            self.log.exception(f"ConnectionError: Couldn't connect to: {url}")
-        except requests.HTTPError as e:
-            self.log.exception(str(e))
-            raise
-        except requests.exceptions.RequestException as e:  # All other types of exceptions
-            self.log.exception(str(e))
-            raise
-        except Exception:
-            raise
-
-        # File has been scanned, if response is successful, let's get the response
-        if json_response is not None and json_response.get('response_code') <= 0:
-            return json_response
-
-        sha256 = json_response.get('sha256', 0)
-        if not sha256:
-            return json_response
-
-        # Have to wait for the file scan to be available -- might take a few minutes...
-        while True:
-            url = self.config.get("base_url") + "file/report"
-            params = dict(
-                apikey=api_key or self.api_key,
-                resource=sha256,
-            )
-
-            json_response = None
+        with open(filename, "rb") as file_obj:
             try:
-                r = requests.post(url, params=params)
-                r.raise_for_status()
-
-                if r.ok:
-                    json_response = r.json()
-                elif r.status_code == 204:
-                    message = "You exceeded the public API request rate limit (4 requests of any nature per minute)"
-                    raise VTException(message)
-            except requests.ConnectionError:
-                self.log.exception(f"ConnectionError: Couldn't connect to: {url}")
-            except requests.HTTPError as e:
-                self.log.exception(str(e))
-                raise
-            except requests.exceptions.RequestException as e:  # All other types of exceptions
-                self.log.exception(str(e))
-                raise
-            except Exception:
-                raise
-
-            if 'scans' in json_response:
-                break
-            # Limit is 4 public API calls per minute, make sure we don't exceed quota
-            time.sleep(20)
-
-        return json_response
+                json_response = self.client.scan_file(file=file_obj, wait_for_completion=True).to_dict()
+            except APIError as e:
+                if "NotFoundError" in e.code:
+                    self.log.warning("VirusTotal has nothing on this file.")
+                elif "QuotaExceededError" in e.code:
+                    self.log.warning("Quota Exceeded. Trying again in 60s")
+                    time.sleep(60)
+                    return self.scan_file(request)
+                else:
+                    self.log.error(e)
+            return json_response
 
     @staticmethod
     def parse_results(response: Dict[str, Any]):
         res = Result()
-        response = response.get('results', response)
 
-        if response is not None and response.get('response_code') == 204:
-            message = "You exceeded the public API request rate limit (4 requests of any nature per minute)"
-            raise VTException(message)
-        elif response is not None and response.get('response_code') == 203:
-            message = "You tried to perform calls to functions for which you require a Private API key."
-            raise VTException(message)
-        elif response is not None and response.get('response_code') == 1:
-            url_section = ResultSection(
-                'Virus total report permalink',
-                body_format=BODY_FORMAT.URL,
-                body=json.dumps({"url": response.get('permalink')}))
-            res.add_section(url_section)
+        url_section = ResultSection(
+            'VirusTotal report permalink',
+            body_format=BODY_FORMAT.URL,
+            body=json.dumps({"url": f"https://www.virustotal.com/gui/{response['type']}/{response['id']}"}))
+        res.add_section(url_section)
 
-            av_hits = ResultSection('Anti-Virus Detections')
-            scans = response.get('scans', response)
-            av_hits.add_line(f'Found {response.get("positives")} AV hit(s) from {response.get("total")} scans.')
-            for majorkey, subdict in sorted(scans.items()):
-                if subdict['detected']:
-                    virus_name = subdict['result']
-                    av_hit_section = AvHitSection(majorkey, virus_name)
-                    av_hit_section.set_heuristic(1, signature=f'{majorkey}.{virus_name}')
-                    av_hit_section.add_tag('av.virus_name', virus_name)
-                    av_hits.add_subsection(av_hit_section)
+        scans = response['last_analysis_results']
+        av_hits = ResultSection('Anti-Virus Detections')
+        av_hits.add_line(f'Found {response["last_analysis_stats"]["malicious"]} AV hit(s) from '
+                         f'{len(response["last_analysis_results"].keys())}')
+        for majorkey, subdict in sorted(scans.items()):
+            if subdict['category'] == "malicious":
+                virus_name = subdict['result']
+                av_hit_section = AvHitSection(majorkey, virus_name)
+                av_hit_section.set_heuristic(1, signature=f'{majorkey}.{virus_name}')
+                av_hit_section.add_tag('av.virus_name', virus_name)
+                av_hits.add_subsection(av_hit_section)
 
-            res.add_section(av_hits)
+        res.add_section(av_hits)
 
         return res
